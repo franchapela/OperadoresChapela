f9d29f8fc16f70fb9a839a7a23232d737edda8fd
Francisco <franciscochapela111@homtail.com>
2022-07-30 05:35:25 -0300

    Subida de clase 4, simulador login/register

--- app.js
+++ app.js
@@ -1,42 +1,68 @@
-let num = 0;
-let num2 = 0;
-let num3 = 1;
-const operacion = prompt("Ingrese la operación a realizar, sumar, restar, dividir(Sólo 2 valores), multiplicar:");
-const nums = prompt("Cantidad de números a " + operacion);
-
-switch(operacion){
-    case "sumar":
-        for(let i = 0; i<nums; i++){
-            num2 = Number(prompt("Ingrese un número:"));
-            num = num + num2;
+let ruser;
+let remail;
+let rpassword;
+const register = () => {
+        ruser = prompt("Ingresa tu nombre de usuario: ");
+        remail = prompt("Ingresa tu correo: ");
+        rpassword;
+    let confpass;
+    do{
+        rpassword = prompt("Ingresa tu contraseña: ");
+        confpass = prompt("Vuelve a introducir tu contraseña: ");
+        if(rpassword != confpass){
+            alert("Las contraseñas no coinciden, vuelva a intentar.")
         }
-        alert("El resultado es: " + num);
-        break;
-    case "restar":
-        for(let i = 0; i<nums; i++){
-            num = Number(prompt("Ingrese un número:"));
-            num = num - num2; //Resto 0 al primer número ingresado para luego poder guardarlo.
-            num2 = num; //Guardo el número ingresado para poder restarlo con el próximo.
-        }
-        alert("El resultado es: " + (num*-1));
-        break;
-    case "dividir":
-        for(let i = 0; i<nums; i++){
-            num = Number(prompt("Ingrese un número:"));
-            num2 = num3/num; //En la primer vuelta no cumple función, luego va a invertir la operación de "num" para realizar una correcta división.
-            num = num/num3; //Divido num por 1(num3), así me devuelve el valor de num la primera vez y lo puedo almacenar correctamente.
-            num3 = num; //Almaceno el primer num ingresado.
-        }
-        alert("El resultado es: " + num2);
-        break;
-    case "multiplicar":
-        for(let i = 0; i<nums; i++){
-            num = Number(prompt("Ingrese un número:"));
-            num = num3 * num; //Multiplico 1(num3) por el valor ingresado en la primer vuelta, luego el valor ingresado se almacena en num3 y este se va multiplicando por los nuevos valores y actualizándose.
-            num3 = num;
-        }
-        alert("El resultado es: " + num);
-        break;
-    default:
-        alert("La operación solicitada no es válida, por favor recargue la página y vuelva a intentar.");
+    }while(rpassword != confpass);
+    alert("Registro exitoso.");
+    return ruser, remail, rpassword;
 }
+const login = (a, b, c) => {
+    let cont = 0;
+    let luser;
+    let lpass;
+    do{
+        cont++;
+        luser = prompt("Ingresa tu usuario o correo: ");
+        lpass = prompt("Ingresa tu contraseña.");
+        if(((luser != a) && (luser != b)) || (lpass != c)){
+            alert("Usuario o contraseña incorrecto, vuelva a intentarlo.");
+        }
+        else{
+            alert("Ha ingresado con éxito.");
+        }
+        if(cont == 3){
+            alert("Ha fallado demasiados intentos, por favor comuníquese al +54123456 para recibir soporte o acérquese a la sucursal más cercana.");
+            break;
+        }
+    }while(((luser != a) && (luser != b)) || (lpass != c)); 
+}
+const start = () => {
+    let menu;
+    let log;
+    do{
+        menu = prompt("Bienvenido, ¿qué desea realizar?\n1) Ingresar\n2) Registrarse");
+        if(menu == "1"){
+            login(ruser, remail, rpassword);
+        }
+        else if(menu == "2"){
+            register();
+            log = prompt("¿Desea ingresar ahora? \n1) Si \n2) No");
+            if(log == "1"){
+                login(ruser, remail, rpassword);
+            }
+            else if(log == "2"){
+                break;
+            }
+            else{
+                alert("Opción inválida, será redirigido al menú principal.");
+                menu = "3";
+            }
+        }
+        else{
+           alert("Opción inválida, por favor intente otra vez.");
+        }
+    }while((menu != "1") && (menu != "2"));
+    return menu;
+}
+start();
+
